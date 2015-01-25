7a8d012c31daad2c964a4e2614bd46189410f3e2
mimmi20 <t_mueller_stolzenhain@yahoo.de>
2015-01-25 23:47:19 +0100

    update data

---
+++ src/Loader.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * class to load a file from a local or remote source
+ *
+ * Copyright (c) 2012-2014 Thomas Müller
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @category   FileLoader
+ * @package    FileLoader
+ * @copyright  2012-2014 Thomas Müller
+ * @author     Thomas Müller <t_mueller_stolzenhain@yahoo.de>
+ * @license    http://www.opensource.org/licenses/MIT MIT License
+ * @link       https://github.com/mimmi20/wurfl-data/
+ */
+
+namespace UaComparator;
+
+/**
+ * class to load the the content from a data file
+ *
+ * @package    FileLoader
+ * @author     Thomas Müller <t_mueller_stolzenhain@yahoo.de>
+ * @copyright  Copyright (c) 2012-2014 Thomas Müller
+ * @version    1.2
+ * @license    http://www.opensource.org/licenses/MIT MIT License
+ * @link       https://github.com/mimmi20/FileLoader/
+ */
+class Loader
+{
+    /**
+     * loads the the content from a data file
+     *
+     * @param string $wurflKey
+     *
+     * @return array $data
+     */
+    public static function load($wurflKey)
+    {
+        /** @var array $data */
+        $data = require '../data/' . $wurflKey . '.php';
+
+        return $data;
+    }
+}
