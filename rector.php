9992cf54ed4b6ea9556e81866e8a820e20bf2e7c
Thomas Müller <mimmi20@live.de>
2025-01-01 21:36:49 +0100

    fix issues

--- rector.php
+++ rector.php
@@ -1,8 +1,9 @@
 <?php
+
 /**
- * This file is part of the mimmi20/ua-comparator package.
+ * This file is part of the mimmi20/useragent-parser-comparison package.
  *
- * Copyright (c) 2015-2023, Thomas Mueller <mimmi20@live.de>
+ * Copyright (c) 2015-2025, Thomas Mueller <mimmi20@live.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -14,10 +15,10 @@ use Rector\Config\RectorConfig;
 use Rector\DeadCode\Rector\If_\RemoveAlwaysTrueIfConditionRector;
 use Rector\DeadCode\Rector\If_\RemoveDeadInstanceOfRector;
 use Rector\DeadCode\Rector\StaticCall\RemoveParentCallWithoutParentRector;
-use Rector\Php71\Rector\FuncCall\CountOnNullRector;
-use Rector\Php80\Rector\FunctionLike\UnionTypesRector;
 use Rector\Php81\Rector\Array_\FirstClassCallableRector;
 use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
+use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
+use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 
@@ -26,23 +27,19 @@ return static function (RectorConfig $rectorConfig): void {
         __DIR__ . '/src',
     ]);
 
-    // register a single rule
-    // $rectorConfig->rule(\Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector\InlineConstructorDefaultToPropertyRector::class);
-
     $rectorConfig->sets([
         SetList::DEAD_CODE,
-        LevelSetList::UP_TO_PHP_81,
+        LevelSetList::UP_TO_PHP_83,
+        PHPUnitSetList::PHPUNIT_100,
     ]);
 
     $rectorConfig->skip(
         [
-            UnionTypesRector::class,
             NullToStrictStringFuncCallArgRector::class,
             RemoveDeadInstanceOfRector::class,
             FirstClassCallableRector::class,
             RemoveAlwaysTrueIfConditionRector::class,
             RemoveParentCallWithoutParentRector::class,
-            CountOnNullRector::class,
         ],
     );
 };
